b968deec9c70039132e6314d1411480b3e34160d
Junior Arias <jarias@vixicom.local>
2026-09-16 16:54:37 -0400

    Eliminar espacios en news/urls.py

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ from .views import (HomePageView, AboutPageView, PostDetailView,
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('post/<int:pk>  /', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('new/', PostCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
