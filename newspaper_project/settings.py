c85a795a4ba684138564e26a2ea409bda14a0aba
Junior Arias <jarias@vixicom.local>
2026-09-22 10:04:42 -0400

    Modificar cantidad de articulos por pagina

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -161,5 +161,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 2,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 10,
 }
